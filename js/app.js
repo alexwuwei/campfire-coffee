429294308ce5e345d8219e671cbfd59bac9aa25f
Hugo A Guerra <guerrahugoa@gmail.com>
2016-01-12 13:07:29 -0800

    initial commit, contains completed constructor for all stores, along with display testing for newly created object that uses constructor

--- js/app.js
+++ js/app.js
@@ -1,4 +1,66 @@
-//my js scripts will be in here
+//new constructor found below
+function CoffeeShop (minPerHour, maxPerHour,cupsPerCust, poundsPerCust) {
+  this.minPerHour = minPerHour;
+  this.maxPerHour = maxPerHour;
+  this.cupsPerCust = cupsPerCust;
+  this.poundsPerCust = poundsPerCust;
+  this.operatingHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
+  this.hourlyCustArray = []; //stores customer number values for all hours
+  this.hourlyPoundsArray = []; //stores pounds sold per hour
+  this.hourlyCupsArray = []; //stores cups sold per hour; as pounds
+  this.hourlyCupsAsPoundsArray = [];
+
+  //calculates customers per hour
+  this.hourlyCust = function () {
+    for (var time = 0; time < this.operatingHours.length; time++) {
+    var minimum = this.minPerHour;
+    var maximum = this.maxPerHour;
+    var randomNumber = Math.floor(Math.random() * (maximum - minimum) + minimum);
+
+    this.hourlyCustArray.push(randomNumber);
+    console.log("hello");
+  };
+};
+
+  //calculates pounds sold per hour
+  this.poundsSold = function () {
+    for (var time = 0; time < this.operatingHours.length; time++) {
+      var averagePounds = this.poundsPerCust;
+      var hourlyPounds = this.hourlyCustArray[time] * averagePounds;
+
+      this.hourlyPoundsArray.push(hourlyPounds);
+      console.log("hello, its working");
+    };
+  };
+
+//calculates cups sold per hour returns value as cups and value as pounds
+  this.cupsSold = function () {
+    for (var time = 0; time < this.operatingHours.length; time++) {
+    var averageCups = this.cupsPerCust;
+    var poundsPerCup = 0.05;
+    var cupsHourly = this.hourlyCustArray[time] * averageCups;
+    var cupsAsPoundsHourly = cupsHourly * poundsPerCup;
+
+    this.hourlyCupsArray.push(cupsHourly);
+    this.hourlyCupsAsPoundsArray.push(cupsAsPoundsHourly);
+  };
+};
+
+  this.renderStoreName = function () {
+    var storeTitleEl = document.createElement('h2');
+    storeTitleEl.textContent = ("Pike Place Market");
+    document.body.appendChild(storeTitleEl);
+  };
+
+  this.renderCustData = function () {
+    for (var i = 0; i <= this.operatingHours.length - 1; i++) {
+      var hourlyDataEl = document.createElement('p');
+      hourlyDataEl.textContent = (this.operatingHours[i] + ': ' + (this.hourlyPoundsArray[i] + this.hourlyCupsAsPoundsArray[i]) + ' lbs [' + this.hourlyCustArray[i] + ' customers, ' + this.hourlyCupsArray[i] + ' cups (' + this.hourlyCupsAsPoundsArray[i] + ' lbs), ' + this.hourlyPoundsArray[i] + ' lbs to-go]');
+      document.body.appendChild(hourlyDataEl);
+    };
+  };
+}
+
 var pikePlaceMarket = {
   minPerHour: 14,
   maxPerHour: 55,
@@ -412,3 +474,11 @@ websiteSales.poundsSold();
 websiteSales.cupsSold();
 websiteSales.renderStoreName();
 websiteSales.renderCustData();
+
+var everett = new CoffeeShop (23,34,1.2,2.6);
+
+everett.hourlyCust();
+everett.poundsSold();
+everett.cupsSold();
+everett.renderStoreName();
+everett.renderCustData();
